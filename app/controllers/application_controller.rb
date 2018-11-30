4c9a0bbda5d6edb689e5de70835880a4e3054f0a
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2018-11-30 02:42:39 +0000

    Restrict to signed in, fuss with CSS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
     include SessionsHelper
+    before_action :require_signin
+ 
+    private
+ 
+    def require_signin
+        unless signed_in?
+            flash[:error] = "You must sign in to access this section"
+            redirect_to signin_path # halts request cycle
+        end
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_signin, only: [:new, :create]
+  
   def new
     @user = User.new
     @users = User.all
