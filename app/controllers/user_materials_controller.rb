315eb57b4e36dd37319bae707c01bf5b1474ee86
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2018-12-24 16:54:40 +0000

    Get user material to create successfully

--- app/controllers/user_materials_controller.rb
+++ app/controllers/user_materials_controller.rb
@@ -3,4 +3,27 @@ class UserMaterialsController < ApplicationController
         @user = current_user
         @user_materials = User.find(@user.id).user_materials ||= nil
     end
+    
+    def new
+        @user_material = UserMaterial.new
+        @user_material.material = Material.new
+    end
+    
+    def create
+        # binding.pry
+        @user_material = UserMaterial.create(user_material_params)
+        
+        if @user_material.valid?
+            redirect_to user_user_materials_path
+        else
+            render :new
+        end
+    end
+    
+    private
+    
+    def user_material_params
+        params.require(:user_material).permit(:user_id,
+        material_attributes: [:name])
+    end
 end
--- app/models/user_material.rb
+++ app/models/user_material.rb
@@ -1,4 +1,5 @@
 class UserMaterial < ActiveRecord::Base
     belongs_to :user
     belongs_to :material
+    accepts_nested_attributes_for  :material, :reject_if => :all_blank
 end
