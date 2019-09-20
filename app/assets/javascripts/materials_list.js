ac639728ec35d799b5771015f46785e979f25751
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2019-09-20 16:15:15 -0400

    get craft list to show up on click

--- app/assets/javascripts/materials_list.js
+++ app/assets/javascripts/materials_list.js
@@ -1,29 +1,51 @@
-// console.log("Hello from the Materials List JS file!");
+var Craft = class {
+    constructor(obj){
+        this.id = obj.id;
+        this.title = obj.title;
+        this.method = obj.method;
+        this.description = obj.description;
+        this.materials = obj.materials;
+    }
 
-function addCraftListLinks(){
-    if(document.getElementsByClassName("craft-btn")) {
-        const btnList = document.getElementsByClassName("craft-btn");
-        const btnArr = Array.from(btnList);
-        btnArr.map(item => {
-            item.addEventListener('click', (e) => { 
-                e.preventDefault()
-                getCraftList(item.dataset.id);
-            });
-        });
-        // const fetchPromise = fetch('../crafts.json');
-        // fetchPromise.then(response => {
-        //     return response.json();
-        // }).then(crafts => {
-        //     craftsList.innerHTML = listOfCrafts(crafts);
-        //     addCraftDetailsLinks(craftsList.getElementsByClassName('toggle'));
-        // });
+    formatCraftListItem() {
+        return `<li class="pt-20">
+            <span class="red-text">${this.title} (${this.method})</span> <a data-id="${this.id}" id="link-${this.id}" class="toggle" href="../crafts/${this.id}.json" class="underline">Show details</a>
+            <div id="craft-${this.id}"></div>
+            <a href="../crafts/${this.id}" id="button-${this.id}" class="button-gray display-none">See full description</a>
+        </li>`;
     }
 }
 
-function getCraftList(id){
-    // console.log("Getting Craft number " + id);
+function addCraftListLinks(){
+    if(document.getElementsByClassName("craft-btn")) {
+        const btnArr = Array.from(document.getElementsByClassName("craft-btn"));
+        btnArr.map(item => {
+            item.addEventListener('click', (e) => { 
+                e.preventDefault()
+                getMaterialCraftList(item.dataset.id);
+            });
+        });
+    }
+}
+
+function getMaterialCraftList(id){
     const div = document.getElementById(`material-${id}`);
-    div.innerHTML = "Getting Craft number " + id;
+
+    const fetchPromise = fetch(`../materials/${id}.json`);
+    fetchPromise.then(response => {
+        return response.json();
+    }).then(material => {
+        const list = formatMaterialCraftList(material.crafts);
+        div.innerHTML = list;
+    });
+}
+
+function formatMaterialCraftList(crafts){
+    let craftArr = [];
+    crafts.map(craft => {
+        craftArr.push(craft.title);
+    });
+    return craftArr.join(", ");
 }
 
 document.addEventListener("DOMContentLoaded", function(){
