7ab1d9589b92c50a79f822bf81f55b2f0243fd89
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2019-09-23 16:58:40 -0400

    start capturing new stash field values

--- app/assets/javascripts/new_user_material.js
+++ app/assets/javascripts/new_user_material.js
@@ -11,6 +11,20 @@ function createNewUserMaterial(e){
     const request = new XMLHttpRequest();
     request.open("POST", '../user_materials');
     request.send(new FormData(form));
+
+    // only call this if the send works, otherwise throw an error?
+    showNewMaterial();
+}
+
+function showNewMaterial(){
+    const material_id = document.getElementById('user_material_material_id');
+    const selected = material_id.options[material_id.selectedIndex].innerText;
+    const material = document.getElementById('user_material_material_attributes_name').value;
+    const quantity = document.getElementById('user_material_quantity').value;
+
+    console.log(selected);
+    console.log(material);
+    console.log(quantity);
 }
 
 document.addEventListener("DOMContentLoaded", function(){
