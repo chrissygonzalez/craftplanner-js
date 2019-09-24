df8b5d14b0fb25c018a4eec34225cb11753bc02a
chrissygonzalez <chrissygonzalez@gmail.com>
2019-09-23 21:21:46 -0400

    show material and quantity on new stash submit

--- app/assets/javascripts/new_user_material.js
+++ app/assets/javascripts/new_user_material.js
@@ -9,22 +9,40 @@ function createNewUserMaterial(e){
     e.preventDefault();
     const form = document.getElementById('new-user-material');
     const request = new XMLHttpRequest();
+
     request.open("POST", '../user_materials');
     request.send(new FormData(form));
 
-    // only call this if the send works, otherwise throw an error?
     showNewMaterial();
 }
 
 function showNewMaterial(){
+    const errorDiv = document.getElementById('error_explanation');
+    const successDiv = document.getElementById('success');
     const material_id = document.getElementById('user_material_material_id');
-    const selected = material_id.options[material_id.selectedIndex].innerText;
+    let selected = material_id.options[material_id.selectedIndex].innerText;
     const material = document.getElementById('user_material_material_attributes_name').value;
     const quantity = document.getElementById('user_material_quantity').value;
 
-    console.log(selected);
-    console.log(material);
-    console.log(quantity);
+    selected === "Choose a material" ? selected = "" : selected = selected;
+
+    if (selected !== "" && material !== "") {
+        displayMessage(errorDiv, 'Please only enter one material name');
+    } else if (selected === "" && material === "") {
+        displayMessage(errorDiv, 'Please enter a material name');
+    } else if (quantity === "") {
+        displayMessage(errorDiv, 'Please enter a quantity');
+    } else {
+        let item;
+        selected ? item = selected : item = material;
+
+        displayMessage(successDiv, `Added ${quantity} of ${item} to your stash`);
+    }
+}
+
+function displayMessage(div, message){
+    div.classList.replace('display-none', 'display-block');
+    div.innerHTML = `<p>${message}.</p>`;
 }
 
 document.addEventListener("DOMContentLoaded", function(){
